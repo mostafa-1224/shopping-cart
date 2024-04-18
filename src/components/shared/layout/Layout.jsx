d1ea91461c9f55fb21adf3a9fac3c4d96dc48a4e
mostafa-1224 <mustafaabdelkader@paymob.com>
2024-04-18 08:07:39 +0200

    adding cart component and hooking the number of the items in the cart to the nav bar cart icon

--- src/components/shared/layout/Layout.jsx
+++ src/components/shared/layout/Layout.jsx
@@ -1,8 +1,17 @@
+import React from 'react';
 import { Link, Outlet } from "react-router-dom";
 import CartIcon from "../../../assets/cart-icon.png";
+import { useCartContext } from '../../../contexts/CartContext';
 import './layout.scss';
 
 export default function Layout() {
+
+  const { state:cartCtx } = useCartContext();
+  let items = 0;
+  for(let i = 0; i < cartCtx.length; i++){
+    items += cartCtx[i].count;
+  }
+  console.log(items)
   return (
     <div className="layout">
       <div className="header">
@@ -19,14 +28,12 @@ export default function Layout() {
               <Link to="/cart">Cart</Link>
             </li>
             <li className="cart-icon">
-              <img src={CartIcon} alt="" srcset="" width="30px" />
-              <span>0</span>
+              <img src={CartIcon} alt="" width="30px" />
+              <span>{items}</span>
             </li>
           </ul>
         </nav>
       </div>
-
-
       <Outlet />
     </div>
   );
