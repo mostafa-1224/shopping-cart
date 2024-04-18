28d182d438d1c3165b8a70a91b899d02762071a8
mostafa-1224 <mustafaabdelkader@paymob.com>
2024-04-18 08:09:06 +0200

    finalizing the product component and adding all the required features

--- src/components/products/ProductComponent.jsx
+++ src/components/products/ProductComponent.jsx
@@ -1,15 +1,64 @@
 import React from 'react'
+import { useCartContext } from '../../contexts/CartContext'
+import { Link } from 'react-router-dom';
 
-function ProductComponent({item}) {
+function ProductComponent({ item, enableDelete = false }) {
+
+  const { dispatch } = useCartContext();
+
+  const addProduct = () => {
+    console.log("from add to cart")
+    dispatch({
+      type: "ADD_PRODUCT",
+      payload: { ...item }
+    })
+    console.log("from add to cart")
+  }
+  const deleteProduct = () => {
+    dispatch({
+      type: "DELETE_PRODUCT",
+      payload: { ...item }
+    })
+  }
+  const increaseProduct = () => {
+    dispatch({
+      type: "INCREASE_PRODUCT",
+      payload: { ...item }
+    })
+  }
+  const decreaseProduct = () => {
+    dispatch({
+      type: "DECREASE_PRODUCT",
+      payload: { ...item }
+    })
+  }
+
   return (
     <div className='product'>
       <div className="product-details">
-        <img src={item.thumbnail} alt="product image"/>
+        <img src={item.thumbnail} alt="product image" />
         <p>{item.title}</p>
-        <p>{item.price}</p>
+        <p>price £{item.price}</p>
         <p>{item.description}</p>
       </div>
-      <button onClick={() => { console.log(item) }}>Add To Cart</button>
+      <div className="actions">
+        {enableDelete ?
+          <>
+            <div className='controllers'>
+              <span onClick={decreaseProduct}> - </span>
+              <p>{item.count}</p>
+              <span onClick={increaseProduct}> + </span>
+            </div>
+            <button onClick={deleteProduct}>Delete From Cart</button>
+          </>
+          :
+          <>
+            <Link to={`/products/${item.id}`} >View Product Details</Link>
+            <button onClick={addProduct}>Add To Cart</button>
+          </>
+        }
+      </div>
+
     </div>
   )
 }
