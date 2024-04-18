a27102de3fc08f63c04fc72f50b5fdd30d8e7826
mostafa-1224 <mustafaabdelkader@paymob.com>
2024-04-18 06:30:59 +0200

    remove react strict mood

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,9 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+  <BrowserRouter>
+    <CartContextProvider>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+    </CartContextProvider>
+  </BrowserRouter>
 )
